f40919e7d40504a053af7a00d5ff3eb0ac3d2019
swallsy <smwall84@gmail.com>
2017-06-07 09:58:30 -0400

    update 3s and 5s

--- 3s_and_5s/main.js
+++ 3s_and_5s/main.js
@@ -1,24 +1,14 @@
-let sum = 0;
-let number = 0;
 
-// Define a function that loops through numbers 1-100. Runs the number through the isMultiple function. If true, adds to sum.
-function findMultiples(i) {
-  for (i=0; i <= 1000; i++) {
-    if (isMultiple(i)) {
-      sum = sum + i;
+function findMultiples() {
+  let sum = 0;
+  let n = 0;
+  while (n < 1000)  {
+    if (n % 3 === 0 || n % 5 === 0 ) {
+      sum += n;
     }
+     n++;
   }
+  return sum;
 }
 
-// function checks if the number is a multiple of 3 or 5
-
-function isMultiple (n) {
- if (n % 3 === 0 || n % 5 === 0) {
-     return true;
- }  else {
-   return false;
-    }
-}
-
-findMultiples(number);
-console.log(sum);
+findMultiples();
